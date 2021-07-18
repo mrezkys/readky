e8b8c82c8460f4e3836c2fb6a6441ae3afd6f5f6
mrezkys <mrezkysulihin@gmail.com>
2021-07-18 16:11:14 +0800

    create custom bottom navigation bar

---
+++ lib/view/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:readky/view/utils/app_theme.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  int selectedIndex;
+  Function onItemTapped;
+  CustomBottomNavigationBar({@required this.selectedIndex, @required this.onItemTapped});
+
+  @override
+  _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  BottomNavigationBarItem _bottomNavigationBarHomeItemBuilder() {
+    return (widget.selectedIndex == 0)
+        ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Home-filled.svg'), label: 'Home')
+        : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Home.svg'), label: 'Home');
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarSearchItemBuilder() {
+    return (widget.selectedIndex == 1)
+        ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Search-filled.svg'), label: 'Home')
+        : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Search.svg'), label: 'Home');
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarBookmarkItemBuilder() {
+    return (widget.selectedIndex == 2)
+        ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Bookmark-filled.svg'), label: 'Home')
+        : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Bookmark.svg'), label: 'Home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      height: 75,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: AppTheme.softBorderColor, width: 1)),
+        color: Colors.white,
+      ),
+      child: BottomNavigationBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        currentIndex: widget.selectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: widget.onItemTapped,
+        items: <BottomNavigationBarItem>[
+          _bottomNavigationBarHomeItemBuilder(),
+          _bottomNavigationBarSearchItemBuilder(),
+          _bottomNavigationBarBookmarkItemBuilder(),
+        ],
+      ),
+    );
+  }
+}
