e2b717c681f68a2806e239062fcce1a57c67e33e
mrezkys <mrezkysulihin@gmail.com>
2021-07-18 16:11:58 +0800

    create app theme util

---
+++ lib/view/utils/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static LinearGradient textOverlayGradient = LinearGradient(colors: [Colors.black.withOpacity(0.45), Colors.black.withOpacity(0.0)], begin: Alignment.bottomCenter, end: Alignment.topCenter);
+  static Color softBorderColor = Color(0xFFE2E2E2);
+}
