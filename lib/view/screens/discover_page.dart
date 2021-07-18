795e095c258350ab4b9afb0cf5b97f7c4eaf3667
mrezkys <mrezkysulihin@gmail.com>
2021-07-18 17:10:04 +0800

    create discover page

---
+++ lib/view/screens/discover_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:readky/model/core/video_news.dart';
+import 'package:readky/model/helper/video_news_helper.dart';
+import 'package:readky/view/utils/app_theme.dart';
+import 'package:readky/view/widgets/custom_app_bar.dart';
+import 'package:readky/view/widgets/featured_news_card.dart';
+import 'package:readky/view/widgets/featured_video_news_card.dart';
+import 'package:readky/view/widgets/tag_card.dart';
+import 'package:scroll_indicator/scroll_indicator.dart';
+
+class DiscoverPage extends StatefulWidget {
+  @override
+  _DiscoverPageState createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  ScrollController _featuredVideoNewsCardScrollController = ScrollController();
+  List<VideoNews> featuredVideoNews = VideoNewsHelper.featuredVideoNews;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        leadingIcon: SvgPicture.asset(
+          'assets/icons/Menu.svg',
+          color: Colors.white,
+        ),
+        onPressedLeading: () {},
+        title: Text(
+          'Discover',
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/icons/Search.svg',
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            width: MediaQuery.of(context).size.width,
+            height: 240,
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  height: 170,
+                  child: ListView.separated(
+                    controller: _featuredVideoNewsCardScrollController,
+                    padding: EdgeInsets.only(left: 16),
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    itemCount: featuredVideoNews.length,
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        width: 10,
+                      );
+                    },
+                    itemBuilder: (context, index) {
+                      return FeaturedVideoNewsCard(
+                        data: featuredVideoNews[index],
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Video News',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.6),
+                        ),
+                      ),
+                      ScrollIndicator(
+                        scrollController: _featuredVideoNewsCardScrollController,
+                        height: 6,
+                        width: 30,
+                        indicatorWidth: 20,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white.withOpacity(0.3)),
+                        indicatorDecoration: BoxDecoration(color: Colors.white.withOpacity(0.2), borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
