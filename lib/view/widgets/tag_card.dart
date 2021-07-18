db551b94d5796840596a2b790ac142f7e9a3e477
mrezkys <mrezkysulihin@gmail.com>
2021-07-18 17:08:55 +0800

    create tag card widget

---
+++ lib/view/widgets/tag_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TagCard extends StatelessWidget {
+  final String tagName;
+  TagCard({@required this.tagName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      decoration: BoxDecoration(color: Colors.white.withOpacity(0.5), borderRadius: BorderRadius.circular(5)),
+      child: Text(
+        tagName,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
