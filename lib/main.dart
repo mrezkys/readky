175284aa9d72a73f10af30e3ee5d75c5fdb4f0a3
mrezkys <mrezkysulihin@gmail.com>
2021-07-18 16:10:38 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:readky/view/screens/page_switch.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarIconBrightness: Brightness.light,
+  ));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(child: PageSwitch()),
+    );
   }
 }
