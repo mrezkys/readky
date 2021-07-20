29886f855cdbea32a717a5e55077a11440d4094f
mrezkys <mrezkysulihin@gmail.com>
2021-07-20 16:03:09 +0800

    add ontap function to navigate to NewsDetailPage

--- lib/view/widgets/featured_news_card.dart
+++ lib/view/widgets/featured_news_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:readky/model/core/news.dart';
+import 'package:readky/route/slide_page_route.dart';
+import 'package:readky/view/screens/news_detail_page.dart';
 import 'package:readky/view/utils/app_theme.dart';
 import 'package:readky/view/widgets/tag_card.dart';
 
@@ -8,54 +10,59 @@ class FeaturedNewsCard extends StatelessWidget {
   FeaturedNewsCard({this.data});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 320,
-      width: 300,
-      decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.circular(10),
-        image: DecorationImage(
-          image: AssetImage(data.photo),
-          fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(SlidePageRoute(child: NewsDetailPage(data: data)));
+      },
+      child: Container(
+        height: 320,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            image: AssetImage(data.photo),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 320,
-            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 28),
-            decoration: BoxDecoration(gradient: AppTheme.textOverlayGradient),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TagCard(
-                  tagName: 'Movie',
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 14, bottom: 12),
-                  child: Text(
-                    data.title,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 20,
-                      height: 150 / 100,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 320,
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 28),
+              decoration: BoxDecoration(gradient: AppTheme.textOverlayGradient),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TagCard(
+                    tagName: 'Movie',
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 14, bottom: 12),
+                    child: Text(
+                      data.title,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 20,
+                        height: 150 / 100,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  'Jul 14, 2021 11:22am',
-                  style: TextStyle(fontSize: 12, color: Colors.white),
-                )
-              ],
-            ),
-          )
-        ],
+                  Text(
+                    'Jul 14, 2021 11:22am',
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
