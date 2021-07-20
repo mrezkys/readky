6e419fccd374e6084ec387116e998eeff9165674
mrezkys <mrezkysulihin@gmail.com>
2021-07-20 16:06:05 +0800

    remove unused import

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:readky/model/core/news.dart';
 import 'package:readky/model/helper/news_helper.dart';
-import 'package:readky/view/utils/app_theme.dart';
 import 'package:readky/view/widgets/breaking_news_card.dart';
 import 'package:readky/view/widgets/custom_app_bar.dart';
 import 'package:readky/view/widgets/featured_news_card.dart';
