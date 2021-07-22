f96504dca705c235265899198c6636962175c322
mrezkys <mrezkysulihin@gmail.com>
2021-07-22 22:44:46 +0800

    add constructor to SearchAppBar

--- lib/view/widgets/search_app_bar.dart
+++ lib/view/widgets/search_app_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SearchAppBar extends StatefulWidget implements PreferredSizeWidget {
   TextEditingController searchInputController;
-  SearchAppBar({@required this.searchInputController});
+  Function searchPressed;
+  SearchAppBar({@required this.searchInputController, this.searchPressed});
   @override
   Size get preferredSize => const Size.fromHeight(60);
 
@@ -29,10 +31,19 @@ class _SearchAppBarState extends State<SearchAppBar> {
           controller: widget.searchInputController,
           onChanged: (value) {
             print(widget.searchInputController.text);
+            setState(() {});
           },
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
           maxLines: 1,
           decoration: InputDecoration(
+            prefixIcon: Visibility(
+              visible: (widget.searchInputController.text.isEmpty) ? true : false,
+              child: Container(
+                margin: EdgeInsets.only(left: 10, right: 12),
+                child: SvgPicture.asset('assets/icons/Search.svg', color: Colors.white),
+              ),
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20),
             hintText: 'Search...',
             hintStyle: TextStyle(color: Colors.white.withOpacity(0.2)),
             contentPadding: EdgeInsets.only(left: 16, bottom: 9),
@@ -41,17 +52,28 @@ class _SearchAppBarState extends State<SearchAppBar> {
         ),
       ),
       actions: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('cancel'),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.black,
-            textStyle: TextStyle(fontWeight: FontWeight.w400),
-            padding: EdgeInsets.symmetric(horizontal: 20),
-          ),
-        )
+        // The search button will appear if the input is not empty
+        (widget.searchInputController.text.isEmpty)
+            ? ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('cancel'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  textStyle: TextStyle(fontWeight: FontWeight.w400),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                ),
+              )
+            : ElevatedButton(
+                onPressed: widget.searchPressed,
+                child: Text('Search'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  textStyle: TextStyle(fontWeight: FontWeight.w400),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                ),
+              ),
       ],
     );
   }
