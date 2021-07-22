15a2a2b4b4162c14355d9665257157bd1df412ea
mrezkys <mrezkysulihin@gmail.com>
2021-07-22 22:45:12 +0800

    add ui element ; suggestion

--- lib/view/screens/search_page.dart
+++ lib/view/screens/search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:readky/model/core/category.dart';
+import 'package:readky/model/helper/category_helper.dart';
 import 'package:readky/view/widgets/search_app_bar.dart';
 
 class SearchPage extends StatefulWidget {
@@ -8,6 +10,7 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   TextEditingController searchInputController = TextEditingController();
+  List<Category> category = CategoryHelper.categoryData;
   @override
   void dispose() {
     searchInputController.dispose();
@@ -22,6 +25,45 @@ class _SearchPageState extends State<SearchPage> {
       child: Scaffold(
         appBar: SearchAppBar(
           searchInputController: searchInputController,
+          searchPressed: () {},
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 60,
+              color: Colors.black,
+              child: ListView.separated(
+                padding: EdgeInsets.only(left: 16),
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                itemCount: category.length,
+                separatorBuilder: (context, index) {
+                  return SizedBox(width: 16);
+                },
+                itemBuilder: (context, index) {
+                  return Container(
+                    alignment: Alignment.topCenter,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        setState(() {
+                          searchInputController.text = category[index].name;
+                        });
+                      },
+                      child: Text(
+                        category[index].name,
+                        style: TextStyle(color: Colors.white.withOpacity(0.7), fontWeight: FontWeight.w400),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Color(0xFF313131), width: 1),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
