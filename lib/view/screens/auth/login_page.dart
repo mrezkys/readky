8718c8bf57f0f91e45a50edc308a6a58c9002c9f
mrezkys <mrezkysulihin@gmail.com>
2021-07-24 19:23:27 +0800

    create login page

---
+++ lib/view/screens/auth/login_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:readky/route/slide_page_route.dart';
+import 'package:readky/view/screens/page_switch.dart';
+import 'package:readky/view/widgets/custom_text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.black,
+        title: SvgPicture.asset('assets/icons/appname.svg'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: ListView(
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          children: [
+            // Section 1 - Welcome Title
+            Container(
+              margin: EdgeInsets.only(top: 30, bottom: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                "Hii 👋 let's go back to reading. ",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 28, height: 150 / 100),
+              ),
+            ),
+            // Section 2 - Form
+            Container(
+              margin: EdgeInsets.only(bottom: 24),
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomTextField(
+                    hintText: 'youremail@email.com',
+                    labelText: 'Email',
+                  ),
+                  CustomTextField(
+                    labelText: 'Password',
+                    hintText: '********',
+                    obsecureText: true,
+                  ),
+                ],
+              ),
+            ),
+            // Section 3 - Register Button
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 70,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Log in',
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.topCenter,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(SlidePageRoute(child: PageSwitch()));
+                },
+                child: Text('Forgot your password?'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white.withOpacity(0.65),
+                  textStyle: TextStyle(fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
