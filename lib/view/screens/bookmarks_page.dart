d5238fb629b4209c172ac833f756e82f3dfc93ea
mrezkys <mrezkysulihin@gmail.com>
2021-07-25 15:57:23 +0800

    implement openDrawer() and route to SearchPage

--- lib/view/screens/bookmarks_page.dart
+++ lib/view/screens/bookmarks_page.dart
@@ -4,6 +4,8 @@ import 'package:readky/model/core/news.dart';
 import 'package:readky/model/core/video_news.dart';
 import 'package:readky/model/helper/news_helper.dart';
 import 'package:readky/model/helper/video_news_helper.dart';
+import 'package:readky/route/slide_page_route.dart';
+import 'package:readky/view/screens/search_page.dart';
 import 'package:readky/view/widgets/custom_app_bar.dart';
 import 'package:readky/view/widgets/featured_video_news_card.dart';
 import 'package:readky/view/widgets/news_tile.dart';
@@ -32,7 +34,9 @@ class _BookmarkPageState extends State<BookmarkPage> with TickerProviderStateMix
           'assets/icons/Menu.svg',
           color: Colors.white,
         ),
-        onPressedLeading: () {},
+        onPressedLeading: () {
+          Scaffold.of(context).openDrawer();
+        },
         title: Text(
           'Bookmarks',
           style: TextStyle(
@@ -42,7 +46,9 @@ class _BookmarkPageState extends State<BookmarkPage> with TickerProviderStateMix
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(SlidePageRoute(child: SearchPage(), direction: AxisDirection.up));
+            },
             icon: SvgPicture.asset(
               'assets/icons/Search.svg',
               color: Colors.white,
