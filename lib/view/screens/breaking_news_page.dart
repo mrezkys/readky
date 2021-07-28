d5ab136ebecce433f014eb1b3905ed0a21422aaa
mrezkys <mrezkysulihin@gmail.com>
2021-07-28 16:05:02 +0800

    delete commented line

--- lib/view/screens/breaking_news_page.dart
+++ lib/view/screens/breaking_news_page.dart
@@ -33,46 +33,6 @@ class _BreakingNewsPageState extends State<BreakingNewsPage> {
       body: ListView(
         shrinkWrap: true,
         children: [
-          // Container(
-          //   width: MediaQuery.of(context).size.width,
-          //   height: 250,
-          //   alignment: Alignment.topCenter,
-          //   color: Colors.black,
-          //   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-          //   child: Container(
-          //     width: MediaQuery.of(context).size.width,
-          //     height: 200,
-          //     decoration: BoxDecoration(color: Colors.blueGrey, borderRadius: BorderRadius.circular(10), image: DecorationImage(fit: BoxFit.cover, image: AssetImage('assets/images/vaccin.jpg'))),
-          //     child: Container(
-          //       padding: EdgeInsets.symmetric(horizontal: 24, vertical: 30),
-          //       width: MediaQuery.of(context).size.width,
-          //       height: 200,
-          //       decoration: BoxDecoration(gradient: AppTheme.textOverlayGradient),
-          //       child: Column(
-          //         mainAxisAlignment: MainAxisAlignment.end,
-          //         crossAxisAlignment: CrossAxisAlignment.start,
-          //         children: [
-          //           Text(
-          //             'Covid map: Coronavirus cases, deaths, vaccinations by country.',
-          //             maxLines: 3,
-          //             overflow: TextOverflow.ellipsis,
-          //             style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 20),
-          //           ),
-          //           SizedBox(
-          //             height: 5,
-          //           ),
-          //           Text(
-          //             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
-          //             maxLines: 1,
-          //             overflow: TextOverflow.ellipsis,
-          //             style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14),
-          //           )
-          //         ],
-          //       ),
-          //     ),
-          //   ),
-          // )
-
           Container(
             width: MediaQuery.of(context).size.width,
             child: ListView.separated(
