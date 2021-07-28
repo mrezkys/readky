7040f87cc56b0a467ede815c350042944df4d887
mrezkys <mrezkysulihin@gmail.com>
2021-07-28 16:09:52 +0800

    implement onTap function

--- lib/view/widgets/custom_side_bar.dart
+++ lib/view/widgets/custom_side_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:readky/route/slide_page_route.dart';
+import 'package:readky/view/screens/profile_page.dart';
 
 class CustomSideBar extends StatefulWidget {
   @override
@@ -16,43 +18,49 @@ class _CustomSideBarState extends State<CustomSideBar> {
           shrinkWrap: true,
           physics: BouncingScrollPhysics(),
           children: [
-            Container(
-              margin: EdgeInsets.only(top: 30),
-              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-              child: Row(
-                children: [
-                  Container(
-                    width: 48,
-                    height: 48,
-                    margin: EdgeInsets.only(right: 15),
-                    decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(100)),
-                    child: Image.asset(
-                      'assets/images/pp.png',
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(SlidePageRoute(child: ProfilePage()));
+              },
+              child: Container(
+                margin: EdgeInsets.only(top: 30),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                child: Row(
+                  children: [
+                    Container(
                       width: 48,
                       height: 48,
-                      fit: BoxFit.cover,
+                      margin: EdgeInsets.only(right: 15),
+                      decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(100)),
+                      child: Image.asset(
+                        'assets/images/pp.png',
+                        width: 48,
+                        height: 48,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Shasy Rhe',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Text(
-                          'Basic Account.',
-                          style: TextStyle(color: Colors.white.withOpacity(0.35)),
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Shasy Rhe',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            'Basic Account.',
+                            style: TextStyle(color: Colors.white.withOpacity(0.35)),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             Divider(
